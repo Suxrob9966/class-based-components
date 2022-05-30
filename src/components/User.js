ee1a129c2835d0464e0d73f3297de95dafd51c75
Suxrob9966 <rabbimovich9966@gmail.com>
2022-05-30 10:29:45 -0500

    added class based component

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,15 @@
+import { Component } from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// class based componenet can work with functional component. We need extends Component in order to be able to use props.
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
