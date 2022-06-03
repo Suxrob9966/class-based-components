a11e120e6ea2a02b6a192cbf222867fd70a5ee91
Suxrob9966 <rabbimovich9966@gmail.com>
2022-06-02 19:08:20 -0500

    lifecycle methods in action

--- src/components/User.js
+++ src/components/User.js
@@ -3,6 +3,10 @@ import classes from './User.module.css';
 
 // class based componenet can work with functional component. We need extends Component in order to be able to use props.
 class User extends Component {
+  componentWillUnmount() {
+    console.log('User will unmount!');
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
